cb6daad8a3b9d3933417193197da79d950cf5508
sanchezfalco <alexandre.sanchez@student.uclouvain.be>
2013-12-20 12:19:32 +0100

    q1.2 matlab

--- rapport/matlab/methode_graphique.m
+++ rapport/matlab/methode_graphique.m
@@ -2,16 +2,21 @@ function methode_graphique(c,k)
 % Using linear regression to estimate the parameters of a Weibull
 % distribution
 %   
-
+close all
 n = 1000;
 X = sort(wblrnd(c,k,n,1));
 F = (1:n-1)'/n;
 U = log(-log(1-F));
 d = [log(X(1:n-1)) ones(n-1,1)]\U;
-k1 = d(1);
-c1 = exp(d(2)/-k1);
+k1 = d(1)
+c1 = exp(d(2)/-k1)
 
-ERT1 = (k1-k)^2 + (c1-c)^2;
+figure
+plot(log(X(1:n-1)),U,'Color','red','LineWidth',2)
+hold on
+plot(log(X), d(1)*log(X) + d(2),'Color','green','LineWidth',2)
+
+ERT1 = (k1-k)^2 + (c1-c)^2
 
 B = 500;
 ERT = zeros(B,1);
@@ -25,14 +30,13 @@ for i=1:B
     ERT(i) = (K(i)-k)^2 + (C(i)-c)^2;
 end
 
-varE = var(ERT);
-varK = var(K);
-varC = var(C);
-meanE = mean(ERT);
-meanK = mean(K);
-meanC = mean(C);
+varE = var(ERT)
+varK = var(K)
+varC = var(C)
+meanE = mean(ERT)
+meanK = mean(K)
+meanC = mean(C)
 
-close all
 figure
 boxplot(K)
 set(gca,'FontSize',14,'xticklabel',{[]});
