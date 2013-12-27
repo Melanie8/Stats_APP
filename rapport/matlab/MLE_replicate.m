cf0f41f27bc6b84c45dbd6ec681aca7e30c50089
Lena <vicky.peschke@gmail.com>
2013-12-27 14:18:19 +0100

    Modifications 1.3
    Nouvelles figures, nouvel intervalle, question f

--- rapport/matlab/MLE_replicate.m
+++ rapport/matlab/MLE_replicate.m
@@ -26,39 +26,39 @@ figure;
 boxplot(c_MLEs);
 set(gca,'FontSize',14);
 title('Boxplot  pour  c_M_L_E');
-ylabel('c_MLE','Fontsize',14);
+ylabel('c_MLE','fontsize',14);
 
 figure;
 boxplot(k_MLEs);
 set(gca,'FontSize',14);
 title('Boxplot  pour  k_M_L_E');
-ylabel('k_M_L_E','Fontsize',14);
+ylabel('k_M_L_E','fontsize',14);
 
 figure;
 boxplot(ERTs);
 set(gca,'FontSize',14);
 title('Boxplot  pour  ERT_M_L_E');
-ylabel('ERT_M_L_E','Fontsize',14);
+ylabel('ERT_M_L_E','fontsize',14);
 
 figure;
 hist(c_MLEs);
 set(gca,'FontSize',14);
 title ('Histogramme  pour  c_M_L_E');
-xlabel('c_M_L_E','FontSize',14);
-ylabel('nbre  de  c_M_L_E','Fontsize',14);
+xlabel('c_M_L_E','fontsize',14);
+ylabel('nbre  de  c_M_L_E','fontsize',14);
 
 figure;
 hist(k_MLEs);
 set(gca,'FontSize',14);
 title ('Histogramme  pour  k_M_L_E');
-xlabel('k_M_L_E','FontSize',14);
-ylabel('nbre  de  k_M_L_E','Fontsize',14);
+xlabel('k_M_L_E','fontsize',14);
+ylabel('nbre  de  k_M_L_E','fontsize',14);
 
 figure;
 hist(ERTs);
 set(gca,'FontSize',14);
 title ('Histogramme  pour  ERT_M_L_E');
-xlabel('ERT_M_L_E','FontSize',14);
-ylabel('nbre  de  ERT_M_L_E','Fontsize',14);
+xlabel('ERT_M_L_E','fontsize',14);
+ylabel('nbre  de  ERT_M_L_E','fontsize',14);
 
 end
--- rapport/matlab/wblmle.m
+++ rapport/matlab/wblmle.m
@@ -2,7 +2,7 @@ function [c_MLE, k_MLE, ERT] = wblmle(n, c, k)
 % WBLMLE Maximum likelihood estimator for a given Weibull distribution.
 %   The function generates n sample points from the Weibull distribution
 %   with parameters c and k. It then generates 101 candidates for c an k
-%   in a range of [c-5:c+5] and [k-5:k+5], excluding negative values.
+%   in a range of [0.7c:1.3c] and [0.7k:1.3k].
 %   It computes 
 %   Input:
 %       - n : number of points taken randomly from a Weibull distribution
@@ -19,21 +19,15 @@ function [c_MLE, k_MLE, ERT] = wblmle(n, c, k)
 % and shape parameter k.
 X = wblrnd(c,k,[1,n]);
 
-% Lower bound for the candidate points.
-clow = max(c-5,0);
-klow = max(k-5,0);
-
-% Grid of 101 candidate points where c_MLE ranges from c_low:c+5 and k_MLE
-% from k_low:k+5.
-cs = linspace(clow, c+5, 101);
-ks = linspace(klow, k+5, 101);
+% Grid of 101 candidate points where c_MLE and k_MLE.
+cs = linspace(0.7*c, 1.3*c, 101);
+ks = linspace(0.7*k, 1.3*k, 101);
 grid = zeros(101);
 
 for i=1:101
     c_MLE = cs(1,i);
     for j=1:101
         k_MLE = ks(1,j);
-        %grid(i,j) = ll(X,n,c_MLE,k_MLE);
         grid(i,j) = wblloglike(X, c_MLE, k_MLE);
     end
 end
